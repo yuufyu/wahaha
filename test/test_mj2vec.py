42b61a841873b527aee31c07b92b1be1e9341b0c
yuugata <yuugataaki@gmail.com>
2021-12-13 00:38:54 +0900

    add test

---
+++ test/test_mj2vec.py
@@ -0,0 +1,11 @@
+import unittest
+from mj2vec.mj2vec import *
+
+class TestMjai(unittest.TestCase) :
+    def test_mjai_tile(self) :
+        self.assertEqual(mjai_pai_to_tile37("1m"), 1)
+        self.assertEqual(mjai_pai_to_tile37("5m"), 5)
+        self.assertEqual(mjai_pai_to_tile37("5mr"), 0)
+        self.assertEqual(mjai_pai_to_tile34("1m"), 0)
+        self.assertEqual(mjai_pai_to_tile34("5m"), 4)
+        self.assertEqual(mjai_pai_to_tile34("5mr"), 4)
