b0d5b08ae78ee6acac4a25178aafcf970c60f81a
yuugata <yuugataaki@gmail.com>
2022-01-04 14:45:26 +0900

    remove uuid

--- tool/npz2txt.py
+++ tool/npz2txt.py
@@ -4,6 +4,22 @@ import numpy as np
 
 from features.mj2vec import SPARSE_FEATURE_PADDING, PROGRESSION_FEATURE_PADDING, POSSIBLE_FEATURE_PADDING
 
+TRAIN_TOKEN_OFFSET=2 # 0:CLS, 1:SEP
+TRAIN_TOKEN_PADDING = TRAIN_TOKEN_OFFSET+SPARSE_FEATURE_PADDING + PROGRESSION_FEATURE_PADDING + POSSIBLE_FEATURE_PADDING
+TRAIN_TOKEN_MASK    = TRAIN_TOKEN_PADDING + 1
+
+TRAIN_TOKEN_VOCAB_COUNT=TRAIN_TOKEN_PADDING + 2
+
+assert TRAIN_TOKEN_PADDING     == 2 + 1249
+assert TRAIN_TOKEN_VOCAB_COUNT == 2 + 1249 + 2 # CLS, SEP, PADDING, MASK
+
+TRAIN_MAX_TOKEN_LENGTH = 139 # 26(sparse)+81(progression)+32(possible)
+
+
+# Special tokens
+TRAIN_TOKEN_CLS     = 0
+TRAIN_TOKEN_SEP     = 1
+
 def join_num(num_list) :
     return ",".join([str(n) for n in num_list])
 
@@ -42,19 +58,22 @@ def npz2txt_bert(filename) :
     list_length = sparse_list.shape[0]
 
     for i in range(list_length) :
-        sparse = sparse_list[i]
-        # numeric = numeric_list[i] # unused
-        progression = [num + SPARSE_FEATURE_PADDING for num in progression_list[i]]
-        possible =    [num + SPARSE_FEATURE_PADDING + PROGRESSION_FEATURE_PADDING for num in possible_list[i]]
+        sparse      = [num + TRAIN_TOKEN_OFFSET for num in sparse_list[i]]
+        progression = [num + TRAIN_TOKEN_OFFSET + SPARSE_FEATURE_PADDING for num in progression_list[i]]
+        possible    = [num + TRAIN_TOKEN_OFFSET + SPARSE_FEATURE_PADDING + PROGRESSION_FEATURE_PADDING for num in possible_list[i]]
         actual = actual_list[i]
 
-        assert all([0 <= num and num < SPARSE_FEATURE_PADDING for num in sparse]), f"{uuid} : sparse is invalid ({sparse})"
-        assert all([SPARSE_FEATURE_PADDING <= num and num < SPARSE_FEATURE_PADDING + PROGRESSION_FEATURE_PADDING for num in progression]), f"{uuid} : progression is invalid ({progression})"
-        assert all([SPARSE_FEATURE_PADDING + PROGRESSION_FEATURE_PADDING <= num and num < SPARSE_FEATURE_PADDING + PROGRESSION_FEATURE_PADDING + POSSIBLE_FEATURE_PADDING for num in possible]), f"{uuid} : possible is invalid ({possible})"
+        assert all([TRAIN_TOKEN_OFFSET <= num and num < TRAIN_TOKEN_OFFSET + SPARSE_FEATURE_PADDING for num in sparse]), f"{uuid} : sparse is invalid ({sparse})"
+        assert all([TRAIN_TOKEN_OFFSET + SPARSE_FEATURE_PADDING <= num and num < TRAIN_TOKEN_OFFSET + SPARSE_FEATURE_PADDING + PROGRESSION_FEATURE_PADDING for num in progression]), f"{uuid} : progression is invalid ({progression})"
+        assert all([TRAIN_TOKEN_OFFSET + SPARSE_FEATURE_PADDING + PROGRESSION_FEATURE_PADDING <= num and num < TRAIN_TOKEN_OFFSET + SPARSE_FEATURE_PADDING + PROGRESSION_FEATURE_PADDING + POSSIBLE_FEATURE_PADDING for num in possible]), f"{uuid} : possible is invalid ({possible})"
 
+        token_ids = sparse + progression + possible
+        assert len(token_ids) <= TRAIN_MAX_TOKEN_LENGTH
+        
         # Output text format : 
         # [uuid]\t[train_num]\t[label]\n
-        line = uuid + "\t" + join_num(sparse + progression + possible) + "\t" + str(actual)
+        # line = uuid + "\t" + join_num(token_ids) + "\t" + str(actual)
+        line = join_num(token_ids) + "\t" + str(actual)
         print(line)
 
 def main() :
