6982d42096a8176bec705b03b9e80044ae4ad5a4
yuugata <yuugataaki@gmail.com>
2022-01-11 21:55:28 +0900

    wip

--- test/test_mjai_encoder.py
+++ test/test_mjai_encoder.py
@@ -1,26 +1,26 @@
 import unittest
-from tool.preprocess import *
-from mjlegal.mjtypes import Tile, TilesUtil
+from features.mjai_encoder import *
+# from mjlegal.mjtypes import Tile, TilesUtil
 
 class TestMjai(unittest.TestCase) :
     def test_DeltaScores(self) :
-        self.assertEqual(DeltaScores([35000, 39000, 25000], 0).feature(),[210,213] )
+        self.assertEqual(DeltaScores([35000, 39000, 25000], 0).feature(),[213,216] )
     
     def test_Bakaze(self) :
-        self.assertEqual(Bakaze("E").feature(), [15])
+        self.assertEqual(Bakaze("E").feature(), [18])
         
     def test_DoraMarkers(self) :
         tiles = ["1m", "2m", "C", "C"]
-        self.assertEqual(DoraMarkers(TilesUtil.tiles_str_array_to_tiles(tiles)).feature(), [19, 57, 128, 165])
+        self.assertEqual(DoraMarkers(TilesUtil.tiles_str_array_to_tiles(tiles)).feature(), [22, 60, 131, 168])
     
     def test_Rank(self) :
         self.assertEqual(Rank().get_rel_rank([1000,2000,3000], 0), [2,1,0])
         self.assertEqual(Rank().get_rel_rank([1000,2000,3000], 1), [1,0,2])
         self.assertEqual(Rank().get_rel_rank([1000,2000,3000], 2), [0,2,1])
 
-        self.assertEqual(Rank([1000,2000,3000], 0).feature(), [208])
-        self.assertEqual(Rank([1000,2000,3000], 1).feature(), [205])
-        self.assertEqual(Rank([1000,2000,3000], 2).feature(), [204])
+        self.assertEqual(Rank([1000,2000,3000], 0).feature(), [211])
+        self.assertEqual(Rank([1000,2000,3000], 1).feature(), [208])
+        self.assertEqual(Rank([1000,2000,3000], 2).feature(), [207])
 
     def test_Action(self) :
         mjai = {"type" : "dahai", "pai" : "0m", "tsumogiri" : False}
